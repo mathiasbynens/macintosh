69c95bde4f3014a423b66ee1008f4af3a15d3a81
Mathias Bynens <mathias@qiwi.be>
2014-05-15 15:52:52 +0200

    Tag the v0.1.2 release

--- macintosh.js
+++ macintosh.js
@@ -1,4 +1,4 @@
-/*! http://mths.be/macintosh v0.1.1 by @mathias | MIT license */
+/*! http://mths.be/macintosh v0.1.2 by @mathias | MIT license */
 ;(function(root) {
 
 	// Detect free variables `exports`.
@@ -121,7 +121,7 @@
 			'macintosh',
 			'x-mac-roman'
 		],
-		'version': '0.1.1'
+		'version': '0.1.2'
 	};
 
 	// Some AMD build optimizers, like r.js, check for specific condition patterns
