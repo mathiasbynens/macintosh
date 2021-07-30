8ca4519a4a6289293bde1a6534d19ad26dc11da0
Mathias Bynens <mathias@qiwi.be>
2021-07-30 12:20:09 +0200

    Rebuild library

--- macintosh.js
+++ macintosh.js
@@ -1,21 +1,5 @@
-/*! https://mths.be/macintosh v1.0.0 by @mathias | MIT license */
-;(function(root) {
-
-	// Detect free variables `exports`.
-	var freeExports = typeof exports == 'object' && exports;
-
-	// Detect free variable `module`.
-	var freeModule = typeof module == 'object' && module &&
-		module.exports == freeExports && module;
-
-	// Detect free variable `global`, from Node.js/io.js or Browserified code,
-	// and use it as `root`.
-	var freeGlobal = typeof global == 'object' && global;
-	if (freeGlobal.global === freeGlobal || freeGlobal.window === freeGlobal) {
-		root = freeGlobal;
-	}
-
-	/*--------------------------------------------------------------------------*/
+/*! https://mths.be/macintosh v1.0.1 by @mathias | MIT license */
+;(function() {
 
 	var object = {};
 	var hasOwnProperty = object.hasOwnProperty;
@@ -33,7 +17,7 @@
 			return '&#' + codePoint + ';';
 		}
 		// Else, `mode == 'fatal'`.
-		throw Error();
+		throw new Error();
 	};
 
 	// https://encoding.spec.whatwg.org/#single-byte-decoder
@@ -113,37 +97,17 @@
 	};
 
 	var macintosh = {
-		'encode': encode,
-		'decode': decode,
-		'labels': [
+		encode: encode,
+		decode: decode,
+		labels: [
 			'csmacintosh',
 			'mac',
 			'macintosh',
 			'x-mac-roman'
 		],
-		'version': '1.0.0'
+		version: '1.0.1',
 	};
 
-	// Some AMD build optimizers, like r.js, check for specific condition patterns
-	// like the following:
-	if (
-		typeof define == 'function' &&
-		typeof define.amd == 'object' &&
-		define.amd
-	) {
-		define(function() {
-			return macintosh;
-		});
-	}	else if (freeExports && !freeExports.nodeType) {
-		if (freeModule) { // in Node.js, io.js or RingoJS v0.8.0+
-			freeModule.exports = macintosh;
-		} else { // in Narwhal or RingoJS v0.7.0-
-			for (var key in macintosh) {
-				macintosh.hasOwnProperty(key) && (freeExports[key] = macintosh[key]);
-			}
-		}
-	} else { // in Rhino or a web browser
-		root.macintosh = macintosh;
-	}
+	module.exports = macintosh;
 
-}(this));
+}());
--- tests/tests.js
+++ tests/tests.js
@@ -1,158 +1,111 @@
-(function(root) {
-	'use strict';
+const assert = require('assert');
 
-	var noop = Function.prototype;
+const macintosh = require('../macintosh.js');
 
-	var load = (typeof require == 'function' && !(root.define && define.amd)) ?
-		require :
-		(!root.document && root.java && root.load) || noop;
+console.log('Testing `macintosh.encode`…');
+assert.strictEqual(
+	macintosh.encode('\0\x01\x02\x03\x04\x05\x06\x07\b\t\n\x0B\f\r\x0E\x0F\x10\x11\x12\x13\x14\x15\x16\x17\x18\x19\x1A\x1B\x1C\x1D\x1E\x1F !"#$%&\'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_`abcdefghijklmnopqrstuvwxyz{|}~\x7F'),
+	'\0\x01\x02\x03\x04\x05\x06\x07\b\t\n\x0B\f\r\x0E\x0F\x10\x11\x12\x13\x14\x15\x16\x17\x18\x19\x1A\x1B\x1C\x1D\x1E\x1F !"#$%&\'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_`abcdefghijklmnopqrstuvwxyz{|}~\x7F',
+	'U+0000 to U+007F remain unchanged'
+);
+assert.strictEqual(
+	macintosh.encode('\xC4\xC5\xC7\xC9\xD1\xD6\xDC\xE1\xE0\xE2\xE4\xE3\xE5\xE7\xE9\xE8\xEA\xEB\xED\xEC\xEE\xEF\xF1\xF3\xF2\xF4\xF6\xF5\xFA\xF9\xFB\xFC\u2020\xB0\xA2\xA3\xA7\u2022\xB6\xDF\xAE\xA9\u2122\xB4\xA8\u2260\xC6\xD8\u221E\xB1\u2264\u2265\xA5\xB5\u2202\u2211\u220F\u03C0\u222B\xAA\xBA\u03A9\xE6\xF8\xBF\xA1\xAC\u221A\u0192\u2248\u2206\xAB\xBB\u2026\xA0\xC0\xC3\xD5\u0152\u0153\u2013\u2014\u201C\u201D\u2018\u2019\xF7\u25CA\xFF\u0178\u2044\u20AC\u2039\u203A\uFB01\uFB02\u2021\xB7\u201A\u201E\u2030\xC2\xCA\xC1\xCB\xC8\xCD\xCE\xCF\xCC\xD3\xD4\uF8FF\xD2\xDA\xDB\xD9\u0131\u02C6\u02DC\xAF\u02D8\u02D9\u02DA\xB8\u02DD\u02DB\u02C7'),
+	'\x80\x81\x82\x83\x84\x85\x86\x87\x88\x89\x8A\x8B\x8C\x8D\x8E\x8F\x90\x91\x92\x93\x94\x95\x96\x97\x98\x99\x9A\x9B\x9C\x9D\x9E\x9F\xA0\xA1\xA2\xA3\xA4\xA5\xA6\xA7\xA8\xA9\xAA\xAB\xAC\xAD\xAE\xAF\xB0\xB1\xB2\xB3\xB4\xB5\xB6\xB7\xB8\xB9\xBA\xBB\xBC\xBD\xBE\xBF\xC0\xC1\xC2\xC3\xC4\xC5\xC6\xC7\xC8\xC9\xCA\xCB\xCC\xCD\xCE\xCF\xD0\xD1\xD2\xD3\xD4\xD5\xD6\xD7\xD8\xD9\xDA\xDB\xDC\xDD\xDE\xDF\xE0\xE1\xE2\xE3\xE4\xE5\xE6\xE7\xE8\xE9\xEA\xEB\xEC\xED\xEE\xEF\xF0\xF1\xF2\xF3\xF4\xF5\xF6\xF7\xF8\xF9\xFA\xFB\xFC\xFD\xFE\xFF',
+	'Encoding all other symbols in the character set'
+);
+assert.throws(
+	() => {
+		macintosh.encode('\uFFFF');
+	},
+	Error,
+	'Encoding a code point that is invalid for this encoding throws an error in `fatal` mode, which is the implied default for `encode()`'
+);
+assert.throws(
+	() => {
+		macintosh.encode('\uFFFF', { mode: 'fatal' });
+	},
+	Error,
+	'Encoding a code point that is invalid for this encoding throws an error in `fatal` mode'
+);
+assert.throws(
+	() => {
+		macintosh.encode('\uFFFF', { mode: 'FATAL' });
+	},
+	Error,
+	'Mode names are case-insensitive'
+);
+assert.throws(
+	() => {
+		macintosh.encode('\uFFFF', { mode: 'fAtAl' });
+	},
+	Error,
+	'Mode names are case-insensitive'
+);
+assert.strictEqual(
+	macintosh.encode('\uFFFF', { mode: 'html' }),
+	'&#65535;',
+	'Encoding a code point that is invalid for this encoding returns an HTML entity in `html` mode'
+);
+assert.strictEqual(
+	macintosh.encode('\uFFFF', { mode: 'HTML' }),
+	'&#65535;',
+	'Mode names are case-insensitive'
+);
+assert.strictEqual(
+	macintosh.encode('\uFFFF', { mode: 'hTmL' }),
+	'&#65535;',
+	'Mode names are case-insensitive'
+);
 
-	var QUnit = (function() {
-		return root.QUnit || (
-			root.addEventListener || (root.addEventListener = noop),
-			root.setTimeout || (root.setTimeout = noop),
-			root.QUnit = load('../node_modules/qunitjs/qunit/qunit.js') || root.QUnit,
-			addEventListener === noop && delete root.addEventListener,
-			root.QUnit
-		);
-	}());
-
-	var qe = load('../node_modules/qunit-extras/qunit-extras.js');
-	if (qe) {
-		qe.runInContext(root);
-	}
-
-	// The `macintosh` object to test
-	var macintosh = root.macintosh || (root.macintosh = (
-		macintosh = load('../macintosh.js') || root.macintosh,
-		macintosh = macintosh.macintosh || macintosh
-	));
-
-	/*--------------------------------------------------------------------------*/
-
-	// `throws` is a reserved word in ES3; alias it to avoid errors
-	var raises = QUnit.assert['throws'];
-
-	// explicitly call `QUnit.module()` instead of `module()`
-	// in case we are in a CLI environment
-	QUnit.module('macintosh');
-
-	test('macintosh.encode', function() {
-		equal(
-			macintosh.encode('\0\x01\x02\x03\x04\x05\x06\x07\b\t\n\x0B\f\r\x0E\x0F\x10\x11\x12\x13\x14\x15\x16\x17\x18\x19\x1A\x1B\x1C\x1D\x1E\x1F !"#$%&\'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_`abcdefghijklmnopqrstuvwxyz{|}~\x7F'),
-			'\0\x01\x02\x03\x04\x05\x06\x07\b\t\n\x0B\f\r\x0E\x0F\x10\x11\x12\x13\x14\x15\x16\x17\x18\x19\x1A\x1B\x1C\x1D\x1E\x1F !"#$%&\'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_`abcdefghijklmnopqrstuvwxyz{|}~\x7F',
-			'U+0000 to U+007F remain unchanged'
-		);
-		equal(
-			macintosh.encode('\xC4\xC5\xC7\xC9\xD1\xD6\xDC\xE1\xE0\xE2\xE4\xE3\xE5\xE7\xE9\xE8\xEA\xEB\xED\xEC\xEE\xEF\xF1\xF3\xF2\xF4\xF6\xF5\xFA\xF9\xFB\xFC\u2020\xB0\xA2\xA3\xA7\u2022\xB6\xDF\xAE\xA9\u2122\xB4\xA8\u2260\xC6\xD8\u221E\xB1\u2264\u2265\xA5\xB5\u2202\u2211\u220F\u03C0\u222B\xAA\xBA\u03A9\xE6\xF8\xBF\xA1\xAC\u221A\u0192\u2248\u2206\xAB\xBB\u2026\xA0\xC0\xC3\xD5\u0152\u0153\u2013\u2014\u201C\u201D\u2018\u2019\xF7\u25CA\xFF\u0178\u2044\u20AC\u2039\u203A\uFB01\uFB02\u2021\xB7\u201A\u201E\u2030\xC2\xCA\xC1\xCB\xC8\xCD\xCE\xCF\xCC\xD3\xD4\uF8FF\xD2\xDA\xDB\xD9\u0131\u02C6\u02DC\xAF\u02D8\u02D9\u02DA\xB8\u02DD\u02DB\u02C7'),
-			'\x80\x81\x82\x83\x84\x85\x86\x87\x88\x89\x8A\x8B\x8C\x8D\x8E\x8F\x90\x91\x92\x93\x94\x95\x96\x97\x98\x99\x9A\x9B\x9C\x9D\x9E\x9F\xA0\xA1\xA2\xA3\xA4\xA5\xA6\xA7\xA8\xA9\xAA\xAB\xAC\xAD\xAE\xAF\xB0\xB1\xB2\xB3\xB4\xB5\xB6\xB7\xB8\xB9\xBA\xBB\xBC\xBD\xBE\xBF\xC0\xC1\xC2\xC3\xC4\xC5\xC6\xC7\xC8\xC9\xCA\xCB\xCC\xCD\xCE\xCF\xD0\xD1\xD2\xD3\xD4\xD5\xD6\xD7\xD8\xD9\xDA\xDB\xDC\xDD\xDE\xDF\xE0\xE1\xE2\xE3\xE4\xE5\xE6\xE7\xE8\xE9\xEA\xEB\xEC\xED\xEE\xEF\xF0\xF1\xF2\xF3\xF4\xF5\xF6\xF7\xF8\xF9\xFA\xFB\xFC\xFD\xFE\xFF',
-			'Encoding all other symbols in the character set'
-		);
-		raises(
-			function() {
-				macintosh.encode('\uFFFF');
-			},
-			Error,
-			'Encoding a code point that is invalid for this encoding throws an error in `fatal` mode, which is the implied default for `encode()`'
-		);
-		raises(
-			function() {
-				macintosh.encode('\uFFFF', { 'mode': 'fatal' });
-			},
-			Error,
-			'Encoding a code point that is invalid for this encoding throws an error in `fatal` mode'
-		);
-		raises(
-			function() {
-				macintosh.encode('\uFFFF', { 'mode': 'FATAL' });
-			},
-			Error,
-			'Mode names are case-insensitive'
-		);
-		raises(
-			function() {
-				macintosh.encode('\uFFFF', { 'mode': 'fAtAl' });
-			},
-			Error,
-			'Mode names are case-insensitive'
-		);
-		equal(
-			macintosh.encode('\uFFFF', { 'mode': 'html' }),
-			'&#65535;',
-			'Encoding a code point that is invalid for this encoding returns an HTML entity in `html` mode'
-		);
-		equal(
-			macintosh.encode('\uFFFF', { 'mode': 'HTML' }),
-			'&#65535;',
-			'Mode names are case-insensitive'
-		);
-		equal(
-			macintosh.encode('\uFFFF', { 'mode': 'hTmL' }),
-			'&#65535;',
-			'Mode names are case-insensitive'
-		);
-	});
-
-	test('macintosh.decode', function() {
-		equal(
-			macintosh.decode('\0\x01\x02\x03\x04\x05\x06\x07\b\t\n\x0B\f\r\x0E\x0F\x10\x11\x12\x13\x14\x15\x16\x17\x18\x19\x1A\x1B\x1C\x1D\x1E\x1F !"#$%&\'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_`abcdefghijklmnopqrstuvwxyz{|}~\x7F'),
-			'\0\x01\x02\x03\x04\x05\x06\x07\b\t\n\x0B\f\r\x0E\x0F\x10\x11\x12\x13\x14\x15\x16\x17\x18\x19\x1A\x1B\x1C\x1D\x1E\x1F !"#$%&\'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_`abcdefghijklmnopqrstuvwxyz{|}~\x7F',
-			'U+0000 to U+007F remain unchanged'
-		);
-		equal(
-			macintosh.decode('\x80\x81\x82\x83\x84\x85\x86\x87\x88\x89\x8A\x8B\x8C\x8D\x8E\x8F\x90\x91\x92\x93\x94\x95\x96\x97\x98\x99\x9A\x9B\x9C\x9D\x9E\x9F\xA0\xA1\xA2\xA3\xA4\xA5\xA6\xA7\xA8\xA9\xAA\xAB\xAC\xAD\xAE\xAF\xB0\xB1\xB2\xB3\xB4\xB5\xB6\xB7\xB8\xB9\xBA\xBB\xBC\xBD\xBE\xBF\xC0\xC1\xC2\xC3\xC4\xC5\xC6\xC7\xC8\xC9\xCA\xCB\xCC\xCD\xCE\xCF\xD0\xD1\xD2\xD3\xD4\xD5\xD6\xD7\xD8\xD9\xDA\xDB\xDC\xDD\xDE\xDF\xE0\xE1\xE2\xE3\xE4\xE5\xE6\xE7\xE8\xE9\xEA\xEB\xEC\xED\xEE\xEF\xF0\xF1\xF2\xF3\xF4\xF5\xF6\xF7\xF8\xF9\xFA\xFB\xFC\xFD\xFE\xFF'),
-			'\xC4\xC5\xC7\xC9\xD1\xD6\xDC\xE1\xE0\xE2\xE4\xE3\xE5\xE7\xE9\xE8\xEA\xEB\xED\xEC\xEE\xEF\xF1\xF3\xF2\xF4\xF6\xF5\xFA\xF9\xFB\xFC\u2020\xB0\xA2\xA3\xA7\u2022\xB6\xDF\xAE\xA9\u2122\xB4\xA8\u2260\xC6\xD8\u221E\xB1\u2264\u2265\xA5\xB5\u2202\u2211\u220F\u03C0\u222B\xAA\xBA\u03A9\xE6\xF8\xBF\xA1\xAC\u221A\u0192\u2248\u2206\xAB\xBB\u2026\xA0\xC0\xC3\xD5\u0152\u0153\u2013\u2014\u201C\u201D\u2018\u2019\xF7\u25CA\xFF\u0178\u2044\u20AC\u2039\u203A\uFB01\uFB02\u2021\xB7\u201A\u201E\u2030\xC2\xCA\xC1\xCB\xC8\xCD\xCE\xCF\xCC\xD3\xD4\uF8FF\xD2\xDA\xDB\xD9\u0131\u02C6\u02DC\xAF\u02D8\u02D9\u02DA\xB8\u02DD\u02DB\u02C7',
-			'Decoding all other symbols in the character set'
-		);
-		equal(
-			macintosh.decode('\uFFFF'),
-			'\uFFFD',
-			'Decoding a byte that is invalid for this encoding returns U+FFFD in `replacement` mode, which is the implied default for `decode()`'
-		);
-		equal(
-			macintosh.decode('\uFFFF', { 'mode': 'replacement' }),
-			'\uFFFD',
-			'Decoding a byte that is invalid for this encoding returns U+FFFD in `replacement` mode'
-		);
-		equal(
-			macintosh.decode('\uFFFF', { 'mode': 'REPLACEMENT' }),
-			'\uFFFD',
-			'Mode names are case-insensitive'
-		);
-		equal(
-			macintosh.decode('\uFFFF', { 'mode': 'rEpLaCeMeNt' }),
-			'\uFFFD',
-			'Mode names are case-insensitive'
-		);
-		raises(
-			function() {
-				macintosh.decode('\uFFFF', { 'mode': 'fatal' });
-			},
-			Error,
-			'Decoding a byte that is invalid for this encoding throws an error in `fatal` mode'
-		);
-		raises(
-			function() {
-				macintosh.decode('\uFFFF', { 'mode': 'FATAL' });
-			},
-			Error,
-			'Decoding a byte that is invalid for this encoding throws an error in `fatal` mode'
-		);
-		raises(
-			function() {
-				macintosh.decode('\uFFFF', { 'mode': 'fAtAl' });
-			},
-			Error,
-			'Mode names are case-insensitive'
-		);
-	});
-
-	/*--------------------------------------------------------------------------*/
-
-	// configure QUnit and call `QUnit.start()` for
-	// Narwhal, Node.js, PhantomJS, Rhino, and RingoJS
-	if (!root.document || root.phantom) {
-		QUnit.config.noglobals = true;
-		QUnit.start();
-	}
-}(typeof global == 'object' && global || this));
+console.log('Testing `macintosh.decode`…');
+assert.strictEqual(
+	macintosh.decode('\0\x01\x02\x03\x04\x05\x06\x07\b\t\n\x0B\f\r\x0E\x0F\x10\x11\x12\x13\x14\x15\x16\x17\x18\x19\x1A\x1B\x1C\x1D\x1E\x1F !"#$%&\'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_`abcdefghijklmnopqrstuvwxyz{|}~\x7F'),
+	'\0\x01\x02\x03\x04\x05\x06\x07\b\t\n\x0B\f\r\x0E\x0F\x10\x11\x12\x13\x14\x15\x16\x17\x18\x19\x1A\x1B\x1C\x1D\x1E\x1F !"#$%&\'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_`abcdefghijklmnopqrstuvwxyz{|}~\x7F',
+	'U+0000 to U+007F remain unchanged'
+);
+assert.strictEqual(
+	macintosh.decode('\x80\x81\x82\x83\x84\x85\x86\x87\x88\x89\x8A\x8B\x8C\x8D\x8E\x8F\x90\x91\x92\x93\x94\x95\x96\x97\x98\x99\x9A\x9B\x9C\x9D\x9E\x9F\xA0\xA1\xA2\xA3\xA4\xA5\xA6\xA7\xA8\xA9\xAA\xAB\xAC\xAD\xAE\xAF\xB0\xB1\xB2\xB3\xB4\xB5\xB6\xB7\xB8\xB9\xBA\xBB\xBC\xBD\xBE\xBF\xC0\xC1\xC2\xC3\xC4\xC5\xC6\xC7\xC8\xC9\xCA\xCB\xCC\xCD\xCE\xCF\xD0\xD1\xD2\xD3\xD4\xD5\xD6\xD7\xD8\xD9\xDA\xDB\xDC\xDD\xDE\xDF\xE0\xE1\xE2\xE3\xE4\xE5\xE6\xE7\xE8\xE9\xEA\xEB\xEC\xED\xEE\xEF\xF0\xF1\xF2\xF3\xF4\xF5\xF6\xF7\xF8\xF9\xFA\xFB\xFC\xFD\xFE\xFF'),
+	'\xC4\xC5\xC7\xC9\xD1\xD6\xDC\xE1\xE0\xE2\xE4\xE3\xE5\xE7\xE9\xE8\xEA\xEB\xED\xEC\xEE\xEF\xF1\xF3\xF2\xF4\xF6\xF5\xFA\xF9\xFB\xFC\u2020\xB0\xA2\xA3\xA7\u2022\xB6\xDF\xAE\xA9\u2122\xB4\xA8\u2260\xC6\xD8\u221E\xB1\u2264\u2265\xA5\xB5\u2202\u2211\u220F\u03C0\u222B\xAA\xBA\u03A9\xE6\xF8\xBF\xA1\xAC\u221A\u0192\u2248\u2206\xAB\xBB\u2026\xA0\xC0\xC3\xD5\u0152\u0153\u2013\u2014\u201C\u201D\u2018\u2019\xF7\u25CA\xFF\u0178\u2044\u20AC\u2039\u203A\uFB01\uFB02\u2021\xB7\u201A\u201E\u2030\xC2\xCA\xC1\xCB\xC8\xCD\xCE\xCF\xCC\xD3\xD4\uF8FF\xD2\xDA\xDB\xD9\u0131\u02C6\u02DC\xAF\u02D8\u02D9\u02DA\xB8\u02DD\u02DB\u02C7',
+	'Decoding all other symbols in the character set'
+);
+assert.strictEqual(
+	macintosh.decode('\uFFFF'),
+	'\uFFFD',
+	'Decoding a byte that is invalid for this encoding returns U+FFFD in `replacement` mode, which is the implied default for `decode()`'
+);
+assert.strictEqual(
+	macintosh.decode('\uFFFF', { mode: 'replacement' }),
+	'\uFFFD',
+	'Decoding a byte that is invalid for this encoding returns U+FFFD in `replacement` mode'
+);
+assert.strictEqual(
+	macintosh.decode('\uFFFF', { mode: 'REPLACEMENT' }),
+	'\uFFFD',
+	'Mode names are case-insensitive'
+);
+assert.strictEqual(
+	macintosh.decode('\uFFFF', { mode: 'rEpLaCeMeNt' }),
+	'\uFFFD',
+	'Mode names are case-insensitive'
+);
+assert.throws(
+	() => {
+		macintosh.decode('\uFFFF', { mode: 'fatal' });
+	},
+	Error,
+	'Decoding a byte that is invalid for this encoding throws an error in `fatal` mode'
+);
+assert.throws(
+	() => {
+		macintosh.decode('\uFFFF', { mode: 'FATAL' });
+	},
+	Error,
+	'Decoding a byte that is invalid for this encoding throws an error in `fatal` mode'
+);
+assert.throws(
+	() => {
+		macintosh.decode('\uFFFF', { mode: 'fAtAl' });
+	},
+	Error,
+	'Mode names are case-insensitive'
+);
