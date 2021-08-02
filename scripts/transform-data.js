5850a5310f3cdd8b60fbfba7bbd5ab0d2471255c
Mathias Bynens <mathias@qiwi.be>
2021-08-02 11:04:52 +0200

    Make string interpolation more robust

--- scripts/transform-data.js
+++ scripts/transform-data.js
@@ -58,7 +58,7 @@ fs.writeFileSync(
 // tests/tests.src.js → tests/tests.js
 const TEST_TEMPLATE = fs.readFileSync('./tests/tests.src.mjs', 'utf8');
 const createTest = template(TEST_TEMPLATE, {
-	interpolate: /<\%=([\s\S]+?)%\>/g,
+	interpolate: /<%=([\s\S]+?)%>/g,
 });
 const testCode = createTest(require('./export-data.js'));
 fs.writeFileSync('./tests/tests.mjs', testCode);
@@ -66,7 +66,7 @@ fs.writeFileSync('./tests/tests.mjs', testCode);
 // src/macintosh.src.mjs -> macintosh.mjs
 const LIB_TEMPLATE = fs.readFileSync('./src/macintosh.src.mjs', 'utf8');
 const createLib = template(LIB_TEMPLATE, {
-	interpolate: /<\%=([\s\S]+?)%\>/g,
+	interpolate: /<%=([\s\S]+?)%>/g,
 });
 const libCode = createLib(require('./export-data.js'));
 fs.writeFileSync('./macintosh.mjs', libCode);
