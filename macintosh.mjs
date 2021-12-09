ec4ef3623724f3c0280d093d4fac9264983e972c
Mathias Bynens <mathias@qiwi.be>
2021-12-09 15:19:11 +0100

    Release v3.0.4

--- macintosh.mjs
+++ macintosh.mjs
@@ -1,4 +1,4 @@
-/*! https://mths.be/macintosh v3.0.3 by @mathias | MIT license */
+/*! https://mths.be/macintosh v3.0.4 by @mathias | MIT license */
 
 const stringFromCharCode = String.fromCharCode;
 
